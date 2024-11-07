2290be041889d469a31b827ca2ce96d3bf595f03
ohdarlingamber <amberkocks@gmail.com>
2024-11-07 11:22:35 -0500

    JS smooth scrolling for nav links, created back to top button. Added about.html, contact.html, and music.html.

--- script.js
+++ script.js
@@ -29,4 +29,22 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 });
 
+/*
+// Create music links on index.html or music.html
+// Interactive Music Links
+const musicLinks = document.querySelectorAll('#music ul li a');
+
+musicLinks.forEach(link => {
+    link.addEventListener('mouseover', () => {
+        link.style.color = '#f39c12';
+    });
+    link.addEventListener('mouseout', () => {
+        link.style.color = '';
+    });
+    link.addEventListener('click', (e) => {
+        alert(`You clicked on ${link.textContent}!`);
+    });
+});
+
+*/
 
